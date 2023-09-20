a621f16c1d63b6f8bb3affb95893241ac71189e1
Pavlo Moskovchuk <Pavlo.moskovchukgmail.com>
2023-09-20 18:27:42 +0300

    7th class

--- project1/src/main/java/com/springcourse/TestSpring.java
+++ project1/src/main/java/com/springcourse/TestSpring.java
@@ -15,10 +15,23 @@ public class TestSpring {
 
 		//MusicPlayer player = new MusicPlayer(music);
 		MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
+		MusicPlayer player2 = context.getBean("musicPlayer", MusicPlayer.class);
 
-		player.play();
-		System.out.println(player.getName());
+		boolean comparison = player == player2;
+		System.out.println(player);
+		System.out.println(player2);
+
 		System.out.println(player.getVolume());
+		System.out.println(player2.getVolume());
+
+		System.out.println(comparison);
+
+		player.setVolume(80);
+		System.out.println(player.getVolume());
+		System.out.println(player2.getVolume());
+//		player.play();
+//		System.out.println(player.getName());
+//		System.out.println(player.getVolume());
 		context.close();
 	}
 
